41eb2769dfee84588d1ea998d00fb3acbcfbaab3
palmseung <palmseung@gmail.com>
2021-01-05 01:26:45 +0900

    [기본문법] 시작 - JPQL vs Querydsl

--- src/main/java/com/example/querydsl/Member.java
+++ src/main/java/com/example/querydsl/Member.java
@@ -1,22 +1,50 @@
 package com.example.querydsl;
 
+import com.example.querydsl.entity.Team;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "member_id")
   private Long id;
-
-  private String name;
-
+  private String username;
   private Integer age;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id")
+  private Team team;
+
+  public Member(String username) {
+    this(username, 0);
+  }
+
+  public Member(String username, Integer age) {
+    this.username = username;
+    this.age = age;
+  }
+
+  public Member(String username, Integer age, Team team) {
+    this.username = username;
+    this.age = age;
+    this.team = team;
+  }
 }
---
+++ src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,69 @@
+package com.example.querydsl;
+
+import static com.example.querydsl.QMember.member;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.querydsl.entity.Team;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class QuerydslBasicTest {
+
+  private JPAQueryFactory queryFactory;
+
+  @Autowired
+  private EntityManager em;
+
+  @BeforeEach
+  public void before() {
+    queryFactory = new JPAQueryFactory(em);
+
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
+
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
+
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+  }
+
+  @Test
+  void startJPQL() {
+    //find member1 using JPQL
+    Member findMember = em
+        .createQuery("select m from Member m where m.username = :username", Member.class)
+        .setParameter("username", "member1")
+        .getSingleResult();
+
+    assertThat(findMember.getUsername()).isEqualTo("member1");
+  }
+
+  @Test
+  void startQuerydsl() {
+    //find member1 using querydsl
+    Member findMember = queryFactory
+        .select(member)
+        .from(member)
+        .where(member.username.eq("member1"))
+        .fetchOne();
+
+    assertThat(findMember.getUsername()).isEqualTo("member1");
+  }
+}
